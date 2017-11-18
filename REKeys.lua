3b5cd2ab02b31a8419c13fc56b143b5248ae613a
Paweł Jastrzębski <pawelj@iosphe.re>
2017-11-18 22:34:08 +0100

    End of day 1

--- REKeys.lua
+++ REKeys.lua
@@ -2,80 +2,247 @@ local _G = _G
 _G.REKeysNamespace = {}
 local RE = REKeysNamespace
 local LDB = LibStub("LibDataBroker-1.1")
+local QTIP = LibStub("LibQTip-1.0")
 
---GLOBALS: NUM_BAG_SLOTS
-local strsplit, pairs, select, sbyte = _G.strsplit, _G.pairs, _G.select, _G.string.byte
+--GLOBALS: NUM_BAG_SLOTS, RAID_CLASS_COLORS, Game15Font
+local strsplit, pairs, ipairs, select, sbyte, sgsub, time, date, tonumber, fastrandom, wipe, sort, tinsert = _G.strsplit, _G.pairs, _G.ipairs, _G.select, _G.string.byte, _G.string.gsub, _G.time, _G.date, _G.tonumber, _G.fastrandom, _G.wipe, _G.sort, _G.tinsert
+local RegisterAddonMessagePrefix = _G.RegisterAddonMessagePrefix
+local SendAddonMessage = _G.SendAddonMessage
+local GetServerTime = _G.GetServerTime
 local GetContainerNumSlots = _G.GetContainerNumSlots
 local GetContainerItemID = _G.GetContainerItemID
 local GetContainerItemLink = _G.GetContainerItemLink
-local GetRealmName = _G.GetRealmName
-local UnitName = _G.UnitName
+local GetNumFriends = _G.GetNumFriends
+local GetFriendInfo = _G.GetFriendInfo
+local BNGetNumFriends = _G.BNGetNumFriends
+local BNGetFriendInfo = _G.BNGetFriendInfo
+local BNGetGameAccountInfo = _G.BNGetGameAccountInfo
+local UnitFullName = _G.UnitFullName
 local UnitClass = _G.UnitClass
 local UnitFactionGroup = _G.UnitFactionGroup
+local IsInGroup = _G.IsInGroup
+local IsInGuild = _G.IsInGuild
+local IsInRaid = _G.IsInRaid
 local GetMapInfo = _G.C_ChallengeMode.GetMapInfo
+local GetAffixInfo = _G.C_ChallengeMode.GetAffixInfo
+local Timer = _G.C_Timer
+local SecondsToTime = _G.SecondsToTime
 
 RE.Version = 10
+RE.CurrentWeek = 0
+RE.ThrottleTable = {}
+RE.DBNameSort = {}
+RE.DBAltSort = {}
+RE.MyKeyRaw = ""
+
 RE.DefaultSettings = {}
+RE.AffixSchedule = {
+	{6, 3, 9},
+	{5, 13, 10},
+	{7, 12, 9},
+	{8, 3, 10},
+	{11, 2, 9},
+	{5, 14, 10},
+	{6, 4, 9},
+	{7, 2, 10},
+	{5, 4, 9},
+	{8, 12, 10},
+	{7, 13, 9},
+	{11, 14, 10},
+}
 
 -- Event functions
 
 function RE:OnLoad(self)
 	self:RegisterEvent("ADDON_LOADED")
 	self:RegisterEvent("PLAYER_ENTERING_WORLD")
+	self:RegisterEvent("CHAT_MSG_ADDON")
 end
 
-function RE:OnEvent(self, event, name)
+function RE:OnEvent(self, event, name, ...)
 	if event == "ADDON_LOADED" and name == "REKeys" then
 		if not _G.REKeysDB then _G.REKeysDB = {} end
 		if not _G.REKeysSettings then _G.REKeysSettings = RE.DefaultSettings end
+		RE.DB = _G.REKeysDB
+		RE.Settings = _G.REKeysSettings
 		for key, value in pairs(RE.DefaultSettings) do
 			if RE.Settings[key] == nil then
 				RE.Settings[key] = value
 			end
 		end
-		RE.DB = _G.REKeysDB
-		RE.Settings = _G.REKeysSettings
+		if not RE.Settings.ID then
+			RE.Settings.ID = fastrandom(1, 1000000000)
+		end
 
 		--TODO: Drop old records from DB
 
 		RE.LDB = LDB:NewDataObject("REKeys", {
 			type = "data source",
-			text = "REKeys",
+			text = "|cFF74D06CRE|rKeys",
 			icon = "Interface\\Icons\\INV_Relics_Hourglass",
 		})
+		function RE.LDB:OnEnter()
+			RE.Tooltip = QTIP:Acquire("REKeysTooltip", 5, "CENTER", "CENTER", "CENTER", "CENTER", "CENTER")
+			RE.Tooltip:SetHeaderFont(Game15Font)
+			RE:RequestKeys()
+			RE:FillTooltip()
+			RE.Tooltip:SmartAnchorTo(self)
+			RE.Tooltip:Show()
+		end
+		function RE.LDB:OnLeave()
+			QTIP:Release(RE.Tooltip)
+			RE.Tooltip = nil
+		end
+		RegisterAddonMessagePrefix("REKeys")
 
 		self:UnregisterEvent("ADDON_LOADED")
 	elseif event == "PLAYER_ENTERING_WORLD" then
-		RE.MyName = UnitName("player").."-"..GetRealmName()
+		RE.MyName, RE.MyRealm = UnitFullName("player")
+		RE.MyFullName = RE.MyName.."-"..RE.MyRealm
 		RE.MyFaction = UnitFactionGroup("player")
 		RE.MyClass = select(2, UnitClass("player"))
-		RE.MyKey = RE:FindKey()
+		Timer.After(15, RE.KeySearchDelay)
 
 		self:UnregisterEvent("PLAYER_ENTERING_WORLD")
-		self:RegisterEvent("BAG_UPDATE")
-	elseif event == "BAG_UPDATE" then
-		RE.MyKey = RE:FindKey()
+	elseif event == "BAG_NEW_ITEMS_UPDATED" then
+		self:UnregisterEvent("BAG_NEW_ITEMS_UPDATED")
+		RE:FindKey()
+		self:RegisterEvent("BAG_NEW_ITEMS_UPDATED")
+	elseif event == "CHAT_MSG_ADDON" and name == "REKeys" then
+		local msg, _, sender = ...
+		msg = {strsplit(";", msg)}
+		-- TODO: Catch outdated addon
+		if tonumber(msg[2]) == RE.Version and sender ~= RE.MyFullName then
+			if msg[1] == "DajKamienia" and RE.MyKey.DungeonID ~= "0" then
+				print("DajKamienia")
+				if not RE.ThrottleTable[sender] then RE.ThrottleTable[sender] = 0 end
+				local timestamp = time(date('!*t', GetServerTime()))
+				if timestamp - RE.ThrottleTable[sender] > 30 then
+					RE.ThrottleTable[sender] = timestamp
+					SendAddonMessage("REKeys", "MaszKamienia;"..RE.Version..";"..RE.MyFullName..";"..RE.MyClass..";"..RE.MyKey.DungeonID..";"..RE.MyKey.DungeonLevel..";"..RE.Settings.ID, "WHISPER", sender)
+				end
+			elseif msg[1] == "MaszKamienia" then
+				print("MaszKamienia")
+				if not RE.DB[msg[3]] then RE.DB[msg[3]] = {} end
+				RE.DB[msg[3]] = {RE.Version, time(date('!*t', GetServerTime())), msg[4], msg[5], msg[6], msg[7]}
+				if QTIP:IsAcquired("REKeysTooltip") then RE:FillTooltip() end
+			end
+		end
 	end
 end
 
+-- Main functions
+
 function RE:FindKey()
-	local keystone = {["DungeonID"] = 0, ["DungeonLevel"] = 0, ["Prefix1"] = 0, ["Prefix2"] = 0, ["Prefix3"] = 0}
+	print("FindKey")
+	local rawKey = ""
 	for bag = 0, NUM_BAG_SLOTS do
 		local bagSlots = GetContainerNumSlots(bag)
 		for slot = 1, bagSlots do
 			if GetContainerItemID(bag, slot) == 138019 then
-				local keyData = {strsplit(':', GetContainerItemLink(bag, slot))}
-				keystone = {["DungeonID"] = keyData[2], ["DungeonLevel"] = keyData[3], ["Prefix1"] = keyData[4], ["Prefix2"] = keyData[5], ["Prefix3"] = strsplit('|', keyData[6])}
+				rawKey = GetContainerItemLink(bag, slot)
 				break
 			end
 		end
 	end
-	if keystone.DungeonID > 0 then
-		RE.LDB.text = RE:GetShortMapName(GetMapInfo(keystone.DungeonID)).." +"..keystone.DungeonLevel
-	else
-		RE.LDB.text = "-"
+	if RE.MyKeyRaw ~= rawKey then
+		local keyData = {strsplit(':', rawKey)}
+		RE.MyKey = {["DungeonID"] = keyData[2], ["DungeonLevel"] = keyData[3]}
+		RE.MyKeyRaw = rawKey
+		RE.LDB.text = "|cffe6cc80"..RE:GetShortMapName(GetMapInfo(RE.MyKey.DungeonID)).." +"..RE.MyKey.DungeonLevel.."|r"
+
+		if tonumber(RE.MyKey.DungeonLevel) >= 7 then
+			local affixA, affixB = tonumber(keyData[4]), tonumber(keyData[5])
+			for i, affixes in ipairs(RE.AffixSchedule) do
+				if affixA == affixes[1] and affixB == affixes[2] then
+					RE.CurrentWeek = i
+					break
+				end
+			end
+		end
+	elseif rawKey == "" then
+		RE.MyKey = {["DungeonID"] = "0", ["DungeonLevel"] = "0"}
+		RE.LDB.text = "|cffe6cc80-|r"
 	end
-	return keystone
+	if not RE.DB[RE.MyFullName] then RE.DB[RE.MyFullName] = {} end
+	RE.DB[RE.MyFullName] = {RE.Version, time(date('!*t', GetServerTime())), RE.MyClass, RE.MyKey.DungeonID, RE.MyKey.DungeonLevel, RE.Settings.ID}
+end
+
+function RE:RequestKeys()
+	print("RequestKeys")
+	if IsInGroup() or IsInRaid() then
+		SendAddonMessage("REKeys", "DajKamienia;"..RE.Version, "RAID")
+	end
+
+	if IsInGuild() then
+		SendAddonMessage("REKeys", "DajKamienia;"..RE.Version, "GUILD")
+	end
+
+	for i = 1, GetNumFriends() do
+		local name, _, _, _, connected = GetFriendInfo(i)
+		if name and connected then
+			SendAddonMessage("REKeys", "DajKamienia;"..RE.Version, "WHISPER", name.."-"..RE.MyRealm)
+		end
+	end
+
+	for i = 1, BNGetNumFriends() do
+		local _, _, _, _, toonName, toonID = BNGetFriendInfo(i)
+		if toonName then
+			local _, toonName, _, realmName, _, faction = BNGetGameAccountInfo(toonID)
+			if faction == RE.MyFaction then
+				SendAddonMessage("REKeys", "DajKamienia;"..RE.Version, "WHISPER", toonName.."-"..sgsub(realmName, " ", ""))
+			end
+		end
+	end
+end
+
+function RE:FillTooltip()
+	RE.Tooltip:Clear()
+	RE.Tooltip:AddLine()
+	RE.Tooltip:SetCell(1, 1, "", nil, nil, nil, nil, nil, nil, nil, 60)
+	RE.Tooltip:SetCell(1, 2, "", nil, nil, nil, nil, nil, nil, 10, 10)
+	RE.Tooltip:SetCell(1, 3, "", nil, nil, nil, nil, nil, nil, nil, 60)
+	RE.Tooltip:SetCell(1, 4, "", nil, nil, nil, nil, nil, nil, 10, 10)
+	RE.Tooltip:SetCell(1, 5, "", nil, nil, nil, nil, nil, nil, nil, 60)
+	RE:GetPrefixes()
+	RE.Tooltip:AddLine()
+	RE.Tooltip:AddSeparator()
+	RE.Tooltip:AddLine()
+	RE.Tooltip:SetColumnLayout(5, "LEFT", "CENTER", "LEFT", "CENTER", "RIGHT")
+	wipe(RE.DBNameSort)
+	wipe(RE.DBAltSort)
+	for name, data in pairs(RE.DB) do
+		local id = data[6]
+		if not RE.DBAltSort[id] then
+			RE.DBAltSort[id] = {}
+			tinsert(RE.DBNameSort, name)
+		else
+			tinsert(RE.DBAltSort[id], name)
+		end
+	end
+	sort(RE.DBNameSort)
+	for i = 1, #RE.DBNameSort do
+		local name = RE.DBNameSort[i]
+		local data = RE.DB[name]
+		RE.Tooltip:AddLine("|c"..RAID_CLASS_COLORS[data[3]].colorStr..strsplit("-", name).."|r", nil, "|cffe6cc80"..RE:GetShortMapName(GetMapInfo(data[4])).." +"..data[5].."|r", nil, "|cff9d9d9d"..RE:GetShortTime(data[2]).."|r")
+		if #RE.DBAltSort[data[6]] > 0 then
+			sort(RE.DBAltSort[data[6]])
+			for z = 1, #RE.DBAltSort[data[6]] do
+				local name = RE.DBAltSort[data[6]][z]
+				local data = RE.DB[name]
+				RE.Tooltip:AddLine("> |c"..RAID_CLASS_COLORS[data[3]].colorStr..strsplit("-", name).."|r", nil, "|cffe6cc80"..RE:GetShortMapName(GetMapInfo(data[4])).." +"..data[5].."|r", nil, "|cff9d9d9d"..RE:GetShortTime(data[2]).."|r")
+			end
+		end
+	end
+	local fill = false
+	for i = 8, RE.Tooltip:GetLineCount() do
+		if fill then
+			RE.Tooltip:SetLineColor(i, 0, 0, 0, 0.3)
+			fill = false
+		else
+			fill = true
+		end
+	end
 end
 
 -- Support functions
@@ -84,9 +251,32 @@ function RE:GetShortMapName(mapName)
 	local mapNameTemp = {strsplit(" ", mapName)}
 	local mapShortName = ""
 	for i=1, #mapNameTemp do
-		mapShortName = mapShortName..RE:StrSub(mapNameTemp[i],0,1)
+		mapShortName = mapShortName..RE:StrSub(mapNameTemp[i], 0, 1)
 	end
 	return mapShortName
+end
+
+function RE:GetShortTime(dbTime)
+	local rawTime = time(date('!*t', GetServerTime())) - dbTime
+	return SecondsToTime(rawTime)
+end
+
+function RE:GetPrefixes()
+	local affixes = {{0, 0, 0}, {0, 0, 0}}
+	if RE.CurrentWeek > 0 then
+		local scheduleWeek = RE.CurrentWeek - 1 % #RE.AffixSchedule + 1
+		affixes[1] = RE.AffixSchedule[scheduleWeek]
+		scheduleWeek = RE.CurrentWeek % #RE.AffixSchedule + 1
+		affixes[2] = RE.AffixSchedule[scheduleWeek]
+	end
+	RE.Tooltip:AddHeader(GetAffixInfo(affixes[1][1]) or "?", "|cffff0000|||r", GetAffixInfo(affixes[1][2]) or "?", "|cffff0000|||r", GetAffixInfo(affixes[1][3]) or "?")
+	RE.Tooltip:AddLine()
+	RE.Tooltip:AddHeader(GetAffixInfo(affixes[2][1]) or "?", "|cffff0000|||r", GetAffixInfo(affixes[2][2]) or "?", "|cffff0000|||r", GetAffixInfo(affixes[2][3]) or "?")
+end
+
+function RE:KeySearchDelay()
+	RE:FindKey()
+	_G.REKeys:RegisterEvent("BAG_NEW_ITEMS_UPDATED")
 end
 
 function RE:CSize(char)
