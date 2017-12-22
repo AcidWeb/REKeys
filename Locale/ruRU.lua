47d75fc70e2fc89e76209268edba8d25406805df
Paweł Jastrzębski <pawelj@iosphe.re>
2017-12-22 10:46:58 +0100

    Added ruRU locale

---
+++ Locale/ruRU.lua
@@ -0,0 +1,7 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("REKeys", "ruRU")
+
+L["Addon outdated!"] = "Аддон устарел!"
+L["Collecting keystone data. Please wait 10 seconds."] = "Обновление данных по ключам. Подождите 10 секунд."
+L["My new key"] = "Мой новый ключ"
+L["Pinned characters"] = "Прикрепленные персонажи"
+L["Until updated sending and reciving data will be disabled."] = "До обновления отправка и получение данных будут отключены."
