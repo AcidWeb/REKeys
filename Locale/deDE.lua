2fd62767ecef476efd93d31ae258ad6b4115b909
Paweł Jastrzębski <pawelj@iosphe.re>
2018-07-01 11:42:57 +0200

    Updated build environment

--- Locale/deDE.lua
+++ Locale/deDE.lua
@@ -1,8 +1,4 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("REKeys", "deDE")
 if not L then return end
 
-L["Addon outdated!"] = "Addon veraltet!"
-L["Collecting keystone data. Please wait 10 seconds."] = "Sammle Schlüsselstein Daten. Bitte 10 sekunden warten."
-L["My new key"] = "Mein neuer Schlüssel"
-L["Pinned characters"] = "Angeheftete Charakters"
-L["Until updated sending and reciving data will be disabled."] = "Während des Updaten ist das Senden und Empfangen von Daten deaktiviert."
+--@localization(locale="deDE", format="lua_additive_table", handle-unlocalized="ignore")@
--- Locale/ruRU.lua
+++ Locale/ruRU.lua
@@ -1,8 +1,4 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("REKeys", "ruRU")
 if not L then return end
 
-L["Addon outdated!"] = "Аддон устарел!"
-L["Collecting keystone data. Please wait 10 seconds."] = "Обновление данных по ключам. Подождите 10 секунд."
-L["My new key"] = "Мой новый ключ"
-L["Pinned characters"] = "Прикрепленные персонажи"
-L["Until updated sending and reciving data will be disabled."] = "До обновления отправка и получение данных будут отключены."
+--@localization(locale="ruRU", format="lua_additive_table", handle-unlocalized="ignore")@
