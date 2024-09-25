1811be4df0305e6e711487a94cf09835b81c1367
Valdemar <54585769+Hollicsh@users.noreply.github.com>
2024-09-25 11:25:48 +0500

    Update RU locale

--- Locale/ruRU.lua
+++ Locale/ruRU.lua
@@ -2,7 +2,13 @@ local L = LibStub("AceLocale-3.0"):NewLocale("REKeys", "ruRU")
 if not L then return end
 
 L["My new key"] = "Мой новый ключ"
-L["Don't shorten dungeon names"] = "Не сокращайте названия подземелий"
-L["When checked tooltip will display full dungeon name."] = "При проверке всплывающей подсказки будет отображаться полное название подземелья."
-L["Respond to !keys query"] = "Ответить на запрос !Keys"
-L["Pinned characters"] = "Прикрепленные персонажи"
+L["Display offline players"] = "Показать игроков оффлайн"
+L["Don't shorten dungeon names"] = "Не сокращать названия подземелий"
+L["When checked tooltip will display full dungeon name."] = "При установке флажка всплывающая подсказка будет отображать полное название подземелья."
+L["Respond to !keys queries on the guild chat"] = "Реагировать на запросы !keys в чате гильдии"
+L["Respond to !keys queries on the group/raid chat"] = "Реагировать на запросы !keys в чате группы/рейда"
+L["Display minimap button"] = "Отображать кнопку миникарты"
+L["Pinned characters"] = "Закреплённые персонажи"
+L["Comma-separated list of character names. They will be shown at the top of the list regardless of status."] = "Список имен персонажей, разделенных запятыми. Они будут показаны в верхней части списка независимо от статуса."
+L["Send new key notification"] = "Отправлять уведомление о новом ключе"
+L["When checked notification to party will be send when the new key is acquired."] = "Если этот флажок установлен, то при получении нового ключа участнику будет отправлено уведомление."
